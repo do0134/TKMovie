bd53ccbea51de674fc63ba60dfcb50f4d3835899
박진경 <wls0828rud@naver.com>
2022-05-23 01:09:57 +0900

    home and community

--- final-project-front/src/components/NavBar.vue
+++ final-project-front/src/components/NavBar.vue
@@ -1,45 +1,61 @@
 <template>
   <div>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
-      <div class="container-fluid d-flex justify-content-between my-2">
-        <div>
-          <ul class="navbar-nav">
-            <li class="nav-item mx-3">
-              <router-link to="/" class="text-white">Home</router-link>
-            </li>
-            <li class="nav-item mx-3">
-              <router-link to="/articles" class="text-white">Community</router-link>
-            </li>
-            <li class="nav-item mx-3">
-              <router-link to="#" class="text-white">Recommendation</router-link>
-            </li>
-          </ul>
-        </div>
-        <div>
-          <ul class="navbar-nav">
-            <li class="nav-item mx-3">
-              <router-link to="#" class="text-white">Search</router-link>
-            </li>
-            <li class="nav-item mx-3">
-              <router-link to="/login" class="text-white">Login</router-link>
-            </li>
-            <li class="nav-item mx-3">
-              <router-link to="/signup" class="text-white">SignUp</router-link>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-    <!-- <nav class="d-flex justify-content-center align-content-end">
-      <p>바닥</p>
-    </nav> -->
+    <b-navbar toggleable="lg" type="dark" variant="secondary">
+      <b-navbar-brand to="/">Home</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav class="justify-content-between">
+        <b-navbar-nav>
+          <b-nav-item to="/articles">Community</b-nav-item>
+          <b-nav-item to="#">Recommendation</b-nav-item>
+        </b-navbar-nav>
+
+        <b-navbar-nav class="ml-auto">
+          <!-- 검색 버튼 추가하기 -->
+          <!-- <b-nav-form>
+            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+          </b-nav-form> -->
+
+          <b-nav-item to="/login">Login</b-nav-item>
+          <b-nav-item to="/signup">SignUp</b-nav-item>
+
+          <!-- 회원가입 부분을 모달창으로 띄워볼까? -->
+          <!-- <b-modal id="sing-up" hide-footer>
+            <template #modal-title>
+              SignUp
+            </template>
+            <form @submit.prevent="signup(credentials)">
+              <div>
+                <label for="username">Username: </label>
+                <input  v-model="credentials.username" type="text" id="username" required/>
+              </div>
+              <div>
+                <label for="password1">Password: </label>
+                <input v-model="credentials.password1" type="password" id="password1" required />
+              </div>
+              <div>
+                <label for="password2">Password Confirmation:</label>
+                <input v-model="credentials.password2" type="password" id="password2" required />
+              </div>
+              <div>
+                <button>Signup</button>
+              </div>
+            </form>
+            <b-button class="mt-3" block @click="$bvModal.hide('sing-up')">Close Me</b-button>
+          </b-modal> -->
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
   </div>
+
 </template>
 
 <script>
-export default {
+  export default {
 
-}
+  }
 </script>
 
 <style>
--- final-project-front/src/components/PopularMovieCard.vue
+++ final-project-front/src/components/PopularMovieCard.vue
@@ -1,13 +1,34 @@
 <template>
-  <p>가나다라마바사</p>
+  <div>        
+    <div class="card m-3" id="cardimg">    
+      <img :src="imgUrl" class="card-img-top" style="height: 400px; width: 295px;" alt="img">
+        <div class="card-body">
+          <h5 class="card-title fw-bold">{{movie.title}}</h5>
+        </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name : 'PopularMovieCard'
+  name: "PopularMovieCard",
+  props: {
+    movie: Object,
+  },
+  data: function(){
+    return {
+      imgUrl:`https://image.tmdb.org/t/p/w500${this.movie.poster_path}`,
+      }
+  },
 }
+
 </script>
 
 <style>
-
+#cardimg img {
+  transition: all 0.2s linear;
+}
+#cardimg:hover img {
+  transform: scale(1.1);
+}
 </style>
