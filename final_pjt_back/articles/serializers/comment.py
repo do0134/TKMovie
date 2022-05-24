f17de556aee05d84a7c6f0d6b34f0e2b73c19e42
박진경 <wls0828rud@naver.com>
2022-05-24 09:29:03 +0900

    sf

--- final_pjt_back/articles/serializers/comment.py
+++ final_pjt_back/articles/serializers/comment.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from django.conf import settings
+from accounts.models import User
 from ..models import Comment
 
 
@@ -10,7 +10,7 @@ class CommentSerializer(serializers.ModelSerializer):
     
     class UserSerializer(serializers.ModelSerializer):
         class Meta:
-            model = settings.AUTH_USER_MODEL
+            model = User
             fields = ('pk', 'username')
 
     user = UserSerializer(read_only=True)
