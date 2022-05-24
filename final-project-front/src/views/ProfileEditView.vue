bb6df345cf9dccf15c1923cc4b32df4944b4c2db
박진경 <wls0828rud@naver.com>
2022-05-24 17:59:46 +0900

    ff

--- final-project-front/src/views/ProfileEditView.vue
+++ final-project-front/src/views/ProfileEditView.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="container">
-    <h1>여기는 프로필 편집</h1>
-    <b-button variant="link" :to="{ name: 'profile', params: { username } }">
-      <b-icon icon="backspace" aria-hidden="true" variant="dark"></b-icon>
-    </b-button>
-    
+    <h1 class="d-flex my-5">
+      <b-button variant="link" :to="{ name: 'profile', params: { username } }" class="mx-3">
+        <b-icon icon="arrow-left" aria-hidden="true" variant="dark"></b-icon>
+      </b-button>
+      프로필 편집</h1>
     <my-profile></my-profile>
   </div>
 </template>
