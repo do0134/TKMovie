3074042af8bd2aa458bc56456829ff5ccbee9b27
Tony Woo <do0134@Naver.com>
2022-05-24 09:23:22 +0900

    movie_data

--- final_pjt_back/movies/models.py
+++ final_pjt_back/movies/models.py
@@ -1,17 +1,26 @@
 from django.db import models
+from django.conf import settings
+
 
 class Genre(models.Model):
     name = models.CharField(max_length=50)
 
 class Movie(models.Model):
+    movie_id = models.IntegerField()
     title = models.CharField(max_length=100)
-    release_date = models.DateField()
+    released_date = models.DateField()
     popularity = models.FloatField()
-    vote_count = models.IntegerField()
-    vote_average = models.FloatField()
+    vote_avg = models.FloatField()
     overview = models.TextField()
     poster_path = models.CharField(max_length=200)
     genres = models.ManyToManyField(Genre)
-
+    win_worldcup = models.ManyToManyField(settings.AUTH_USER_MODEL,related_name='best_movies')
+    user_score = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='score_by_user')
     def __str__(self):
         return self.title
+
+class Review(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='reviews')
+    title = models.CharField(max_length=100)
+    content = models.TextField()
